1b9c29d2326738a144dc56feca21f31260425172
Ian Berigan <43962789+IanB1717@users.noreply.github.com>
2019-04-05 16:06:29 +0100

    Add files via upload

---
+++ Potentiometer_Termianl_Display.ino
@@ -0,0 +1,17 @@
+int sensorValue;
+int wiperPosition;
+float x;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  sensorValue = analogRead(A0);
+  wiperPosition = map(sensorValue,0, 1023, 0, 270);
+  Serial.println(sensorValue);
+  delay(10);
+}
+
